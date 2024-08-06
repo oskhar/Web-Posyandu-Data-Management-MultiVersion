1807411e786e6abf2ae065b8e978fbf81f7ab8e0
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-08-06 20:53:08 +0700

    chore: install project template

---
+++ react-ts-version/src/configs/primaryColorConfig.ts
@@ -0,0 +1,18 @@
+export type PrimaryColorConfig = {
+  name?: string
+  light?: string
+  main: string
+  dark?: string
+}
+
+// Primary color config object
+const primaryColorConfig: PrimaryColorConfig[] = [
+  {
+    name: 'primary-1',
+    light: '#A379FF',
+    main: '#8C57FF',
+    dark: '#7E4EE6'
+  }
+]
+
+export default primaryColorConfig
